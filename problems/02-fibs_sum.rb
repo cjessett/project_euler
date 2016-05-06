2b4954127f27de06cd958344ea30e05ad44f3f78
CJ Jessett <cjessett@gmail.com>
2016-05-05 23:12:20 -0500

    Add puts line

--- problems/02-fibs_sum.rb
+++ problems/02-fibs_sum.rb
@@ -3,15 +3,15 @@ def fibs(n)
     fibs = []
     fibs << 1 << 2
     idx = 2
-    
+
     while fibs.last < n
         fibs << fibs[idx - 1] + fibs[idx - 2]
         idx += 1
     end
-    
+
     fibs.pop
     fibs = fibs.select { |x| x % 2 == 0 }
     return fibs.inject { |sum,x| sum + x }
 end
 
-fibs(4_000_000)
+puts fibs(4_000_000)
